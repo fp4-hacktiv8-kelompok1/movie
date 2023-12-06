e0b57e1ee5c0d58fd8bf0e97e8ab8fe9c6b16780
usamah <eexvuu@gmail.com>
2023-12-06 12:37:11 +0700

    refactor: implement useReducer

--- src/hooks/useMovieDetail.jsx
+++ src/hooks/useMovieDetail.jsx
@@ -1,32 +1,46 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_START":
+      return { ...state, loading: true, error: false };
+    case "FETCH_SUCCESS":
+      return { ...state, loading: false, error: false, detail: action.payload };
+    case "FETCH_ERROR":
+      return { ...state, loading: false, error: true };
+    default:
+      return state;
+  }
+};
 function useMovieDetail(id) {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-  const [detail, setDetail] = useState([]);
+  const [state, dispatch] = useReducer(reducer, {
+    loading: true,
+    error: false,
+    detail: {},
+  });
 
   useEffect(() => {
-    setLoading(true);
-    setError(false);
-    axios({
-      method: "GET",
-      url: "https://www.omdbapi.com/",
-      params: {
-        apikey: "fd8de445",
-        i: id,
-      },
-    })
-      .then((res) => {
-        setDetail(res.data);
-        setLoading(false);
-      })
-      .catch((e) => {
-        setError(true);
-      });
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_START" });
+      try {
+        const res = await axios.get("https://www.omdbapi.com/", {
+          params: {
+            apikey: "fd8de445",
+            i: id,
+          },
+        });
+
+        dispatch({ type: "FETCH_SUCCESS", payload: res.data });
+      } catch (e) {
+        dispatch({ type: "FETCH_ERROR" });
+      }
+    };
+
+    fetchData();
   }, [id]);
 
-  return { loading, error, detail };
+  return { ...state };
 }
 
 export default useMovieDetail;
--- src/hooks/useMovieSearch.jsx
+++ src/hooks/useMovieSearch.jsx
@@ -1,38 +1,62 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_START":
+      return { ...state, loading: true, error: false };
+    case "FETCH_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        error: false,
+        movies: action.payload.movies,
+        totalPage: Math.ceil(action.payload.totalResults / 10),
+      };
+    case "FETCH_ERROR":
+      return { ...state, loading: false, error: true };
+    default:
+      return state;
+  }
+};
 
 function useMovieSearch(query, pageNumber) {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-  const [totalPage, setTotalPage] = useState(0);
-  const [movies, setMovies] = useState([]);
+  const [state, dispatch] = useReducer(reducer, {
+    loading: true,
+    error: false,
+    totalPage: 0,
+    movies: [],
+  });
 
   useEffect(() => {
-    setLoading(true);
-    setError(false);
-    axios({
-      method: "GET",
-      url: "https://www.omdbapi.com/",
-      params: {
-        apikey: "fd8de445",
-        s: query,
-        page: pageNumber,
-      },
-    })
-      .then((res) => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_START" });
+      try {
+        const res = await axios.get("https://www.omdbapi.com/", {
+          params: {
+            apikey: "fd8de445",
+            s: query,
+            page: pageNumber,
+          },
+        });
+
         if (res.data.Response === "False") {
-          setError(true);
+          dispatch({ type: "FETCH_ERROR" });
+        } else {
+          dispatch({
+            type: "FETCH_SUCCESS",
+            payload: { movies: res.data.Search, totalResults: res.data.totalResults },
+          });
         }
-        setMovies(res.data.Search);
-        setTotalPage(Math.ceil(res.data.totalResults / 10));
-        setLoading(false);
-      })
-      .catch((e) => {
-        setError(true);
-      });
+      } catch (e) {
+        dispatch({ type: "FETCH_ERROR" });
+      }
+    };
+
+    fetchData();
   }, [query, pageNumber]);
 
-  return { loading, error, movies, totalPage };
+  return { ...state };
 }
 
 export default useMovieSearch;
